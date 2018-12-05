644fc8f218980d77c33fc26b72284d12fecdd1f4
srinivasrk <srinivasrk92@gmail.com>
2018-12-05 15:09:54 -0500

    queue bug fix

--- rbmq_receive.py
+++ rbmq_receive.py
@@ -1,17 +1,19 @@
 import pika
 import os
-
+import time
 
 def receive_message():
     credentials = pika.PlainCredentials('srini', 'srini')
-    connection = pika.BlockingConnection(
-    pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', credentials))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', credentials))
     channel = connection.channel()
 
     channel.queue_declare(queue='tasks', durable=True)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
+        time.sleep(2)
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
     # Fair dispatch
     channel.basic_qos(prefetch_count=1)
@@ -19,6 +21,7 @@ def receive_message():
     # send ack
     channel.basic_consume(callback,
                           queue='tasks')
+
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
--- rbmq_send.py
+++ rbmq_send.py
@@ -5,14 +5,19 @@ import os
 def send_message(message_body):
     credentials = pika.PlainCredentials('srini', 'srini')
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', credentials))
+        pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', credentials)
+    )
     channel = connection.channel()
 
     channel.queue_declare(queue='tasks', durable=True)
 
     channel.basic_publish(exchange='',
                           routing_key='tasks',
-                          body=message_body)
+                          body=message_body,
+                          properties=pika.BasicProperties(
+                              delivery_mode=2,  # make message persistent
+                          ))
+
     print(" [x] Sent 'Hello World!'")
 
     connection.close()
